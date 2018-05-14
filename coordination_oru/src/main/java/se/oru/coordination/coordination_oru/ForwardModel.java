fffe93214232f6a8773bcf962e43980d8c8a4890
Marc Hanheide <marc@hanheide.net>
2018-05-14 10:34:47 +0100

    Rebase from 'debian/kinetic/coordination_oru_ros'

--- coordination_oru/src/main/java/se/oru/coordination/coordination_oru/ForwardModel.java
+++
@@ -1,9 +0,0 @@
-package se.oru.coordination.coordination_oru;
-
-import org.metacsp.multi.spatioTemporal.paths.TrajectoryEnvelope;
-
-public interface ForwardModel {
-	
-	public boolean canStop(TrajectoryEnvelope te, RobotReport currentState, int targetPathIndex);
-
-}
